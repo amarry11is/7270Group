d0b2c65dfb65ce4ff017d65c5ec4f5228b689e40
7270comp <zhan.chang.xin@gmail.com>
2024-04-14 20:15:54 +0800

    delete unnessary codes

--- express/routes/users.js
+++ express/routes/users.js
@@ -12,32 +12,6 @@ const { connectToDB, ObjectId } = require('../utils/db');
 router.get('/with/surveys', async function (req, res) {
   const db = await connectToDB();
   try {
-    let result = await db.collection("surveys").aggregate([
-      {
-        $lookup: {
-          from: "surveys",
-          localField: "_id",
-          foreignField: "manager",
-          as: "surveys"
-        }
-      },
-      // remove the ip_address field
-      { $project: { ip_address: 0 } }
-    ]).toArray();
-    res.json(result);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-  finally {
-    await db.client.close();
-  }
-});
-
-const { connectToDB, ObjectId } = require('../utils/db');
-
-router.get('/with/surveys', async function (req, res) {
-  const db = await connectToDB();
-  try {
     let result = await db.collection("users").aggregate([
       {
         $lookup: {
