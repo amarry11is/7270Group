cd48e1a0cae5f924b4743bd58a64e707e025800c
maybelius <maybelius@gmail.com>
2024-04-14 22:22:39 +0800

    mobile

--- mobile/src/views/Tab3Page.vue
+++ mobile/src/views/Tab3Page.vue
@@ -8,40 +8,86 @@
       </ion-card-content>
     </ion-card>
   </ion-page>
+  <div id="chartdiv" style="width:90vw; height:100vh"></div>
 </template>
 
 <script setup>
 import { IonPage, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/vue';
 // import ExploreContainer from '@/components/ExploreContainer.vue';
 import { ref, onMounted } from "vue";
-import { am5 } from "@amcharts/amcharts5";
-import { am5xy } from "@amcharts/amcharts5/xy";
+import * as am5 from "@amcharts/amcharts5";
+import * as am5xy from "@amcharts/amcharts5/xy";
 const items = ref([]);
 
+// Create root and chart
 let root = am5.Root.new("chartdiv");
-let chart = root.container.children.push(
-  am5xy.XYChart.new(root, {})
+let chart = root.container.children.push( 
+  am5xy.XYChart.new(root, {
+    panY: false,
+    layout: root.verticalLayout
+  }) 
 );
 
+// Define data
+let data = [{
+  category: "Research",
+  value1: 1000,
+  value2: 588
+}, {
+  category: "Marketing",
+  value1: 1200,
+  value2: 1800
+}, {
+  category: "Sales",
+  value1: 850,
+  value2: 1230
+}];
+
+// Create Y-axis
 let yAxis = chart.yAxes.push(
   am5xy.ValueAxis.new(root, {
     renderer: am5xy.AxisRendererY.new(root, {})
   })
 );
 
+// Create X-Axis
 let xAxis = chart.xAxes.push(
   am5xy.CategoryAxis.new(root, {
     renderer: am5xy.AxisRendererX.new(root, {}),
     categoryField: "category"
   })
 );
-xAxis.data.setAll([{
-  category: "Research"
-}, {
-  category: "Marketing"
-}, {
-  category: "Sales"
-}];
+xAxis.data.setAll(data);
+
+// Create series
+let series1 = chart.series.push(
+  am5xy.ColumnSeries.new(root, {
+    name: "Series",
+    xAxis: xAxis,
+    yAxis: yAxis,
+    valueYField: "value1",
+    categoryXField: "category"
+  })
+);
+series1.data.setAll(data);
+
+let series2 = chart.series.push(
+  am5xy.ColumnSeries.new(root, {
+    name: "Series",
+    xAxis: xAxis,
+    yAxis: yAxis,
+    valueYField: "value2",
+    categoryXField: "category"
+  })
+);
+series2.data.setAll(data);
+
+// Add legend
+let legend = chart.children.push(am5.Legend.new(root, {}));
+legend.data.setAll(chart.series.values);
+
+// Add cursor
+chart.set("cursor", am5xy.XYCursor.new(root, {}));
 
 
 onMounted(async () => {
--- spa/src/views/LoginView.vue
+++ spa/src/views/LoginView.vue
@@ -51,7 +51,7 @@ const login = async () => {
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
                 <label class="form-check-label" for="exampleCheck1">Check me out</label>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary" @click="login">Submit</button>
         </form>
     </main>
 </template>
